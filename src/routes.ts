3af1ac2c2d7295147f3284fc70007a7cdcbbc95b
abhay113 <abhaychavan346@gmail.com>
2026-01-22 20:48:00 +0530

    revert

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,4 @@ import { authRoutes } from "./modules/auth/auth.route";
 
 export const registerRoutes = (app: FastifyInstance) => {
   app.register(authRoutes, { prefix: "/auth" });  
-  app.get("/health", async () => {
-    return { status: "ok" };
-  });
 };
